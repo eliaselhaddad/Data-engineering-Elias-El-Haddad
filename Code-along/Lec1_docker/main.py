30ac53a96f26a93e6919366ef764277a4da3ab28
eliaselhaddad <86868035+eliaselhaddad@users.noreply.github.com>
2023-05-26 10:21:00 +0200

    lec1 docker fixed

--- Code-along/Lec1_docker/main.py
+++ Code-along/Lec1_docker/main.py
@@ -2,32 +2,34 @@
 import plotly_express as px
 import numpy as np
 from dash import Dash, dcc, Output, Input
-from dash.html import H1, H2, Div, P
+from dash.html import H1, Div, P, H2
 
-dropdown_options = [{
-    "label": f"{rolls} rolls",
-    "value": rolls}
-    for rolls in [10, 100, 1000, 10000]]
+dropdown_options = [{"label": f"{rolls} rolls", "value": rolls} for rolls in [10,100,1000,10000]]
+
 #%%
+
 app = Dash(__name__)
 
 app.layout = Div([
-    H1('Dice simulator'),
-    P('Choose number of dices and number of rolls'),
-    dcc.Graph(id = 'dice-graph'),
-    H2('Number of rolls'),
-    dcc.Dropdown(id = 'number-rolls', options = dropdown_options, value = 10),
-    H2('Number of dices'),
-    dcc.Slider(id = 'number-dices', min = 1, max = 6, value = 2, step = 1),
+    H1("Dice simulator"),
+    P("Choose number of dices and number of rolls and enjoy the results"),
+    dcc.Graph(id = "dice-graph"),
+    H2("Number of rolls"),
+    dcc.Dropdown(id = "number-rolls", options = dropdown_options, value=100),
+    H2("Number of dices"),
+    dcc.Slider(id ="number-dices", min=1, max=6, step=1, value=2, )
 ])
 
-@app.callback(Output('dice-graph', 'figure'), Input('number-rolls', 'value'), Input('number-dices', 'value'))
+@app.callback(Output("dice-graph", "figure"), Input("number-rolls", "value"), Input("number-dices", "value"))
+def _dice_simulator_histogram(rolls, number_dices):
+    dices = np.random.randint(1,7, size=(rolls, number_dices))
+    return px.histogram(dices.sum(axis = 1))
 
-def dice_simulator_histogram(rolls, number_dices):
-    dices = np.random.randint(1, 7, (rolls, number_dices)) # rolls = rows, number_dices = columns
-    return px.histogram(dices.sum(axis = 1)) # sum(axis = 1) = summing over columns
 
-if __name__ == '__main__':
+
+
+
+if __name__ == "__main__":
     print('Hello from the docker side')
 
-    app.run_server(debug=True)
+    app.run_server(host = "0.0.0.0", debug = True, port = 8050)
