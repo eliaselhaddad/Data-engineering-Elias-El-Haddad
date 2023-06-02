218c71e0b3d96c344f6f4dc2d5defe81b61681cd
eliaselhaddad <86868035+eliaselhaddad@users.noreply.github.com>
2023-06-02 10:23:56 +0200

    bash operator

---
+++ Code-along/Lec3_airflow-intro/dags/bash_operator.py
@@ -0,0 +1,23 @@
+from airflow.operators.bash import BashOperator
+from airflow import DAG
+from datetime import datetime
+from pathlib import Path
+
+print(datetime.now())
+
+data_lake_path = Path(__file__).parents[1] / "data" / "data_lake"
+
+data_warehouse_path = Path(__file__).parents[1] / "data" / "data_warehouse"
+
+print(data_lake_path)
+
+time_variable = "(date +%y%m%d_%H%M%S)"
+
+with DAG(dag_id = "joke_DAG", start_date=datetime(2023,6,1)):
+    say_hello = BashOperator(task_id = "say_hello", bash_command="echo 'hej hej joking time'")
+
+    setup_folders = BashOperator(task_id = "setup_folders", bash_command=f"mkdir -p {data_lake_path} {data_warehouse_path}")
+
+    download_joke = BashOperator(task_id = "random_joke", bash_command=f"curl -o {data_lake_path}/joke_{time_variable}.json https://official-joke-api.appspot.com/random_joke")
+
+    say_hello >> setup_folders >> download_joke
