8ba6d1c07b8f05306f06bbf3df1abf77af29f171
eliaselhaddad <86868035+eliaselhaddad@users.noreply.github.com>
2023-08-03 19:29:38 +0200

    lec3

--- Code-along/Lec3_airflow-intro/dags/bash_operator.py
+++ Code-along/Lec3_airflow-intro/dags/bash_operator.py
@@ -2,6 +2,14 @@ from airflow.operators.bash import BashOperator
 from airflow import DAG
 from datetime import datetime
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+API_NINJA_KEY = os.getenv("API_NINJA")
+
+# print(API_NINJA_KEY)
 
 print(datetime.now())
 
@@ -13,11 +21,25 @@ print(data_lake_path)
 
 time_variable = "(date +%y%m%d_%H%M%S)"
 
-with DAG(dag_id = "joke_DAG", start_date=datetime(2023,6,1)):
-    say_hello = BashOperator(task_id = "say_hello", bash_command="echo 'hej hej joking time'")
 
-    setup_folders = BashOperator(task_id = "setup_folders", bash_command=f"mkdir -p {data_lake_path} {data_warehouse_path}")
+with DAG(dag_id="joke_DAG", start_date=datetime(2023, 6, 1)):
+    say_hello = BashOperator(
+        task_id="say_hello", bash_command="echo 'hej hej joking time'"
+    )
 
-    download_joke = BashOperator(task_id = "random_joke", bash_command=f"curl -o {data_lake_path}/joke_{time_variable}.json https://official-joke-api.appspot.com/random_joke")
+    setup_folders = BashOperator(
+        task_id="setup_folders",
+        bash_command=f"mkdir -p {data_lake_path} {data_warehouse_path}",
+    )
+
+    download_joke = BashOperator(
+        task_id="random_joke",
+        bash_command=f"curl -o {data_lake_path}/joke_{time_variable}.json https://official-joke-api.appspot.com/random_joke",
+    )
+
+    download_norris_joke = BashOperator(
+        task_id="chuck_norris_joke",
+        bash_command=f"curl -H {data_lake_path}/norris_joke_{time_variable}.json https://api.chucknorris.io/jokes/random",
+    )
 
     say_hello >> setup_folders >> download_joke
